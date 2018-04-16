6da1ab6579a5480dc939f77998756753949f85cb
Ben Townshend <bentownshend@icloud.com>
2018-04-16 17:35:55 +0100

    added basic drone code and fixed overlapping within the generator

---
+++ PCGDiorama/Assets/Scripts/Drone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drone : MonoBehaviour {
+
+	Generator generator;
+
+	List<Transform> bodyPieces = new List<Transform>();
+	List<Transform> wingPieces = new List<Transform>();
+
+	public void Init(Generator generator) {
+		this.generator = generator;
+
+		TogglePieces(bodyPieces, generator.GetInt(0, bodyPieces.Count));
+		TogglePieces(wingPieces, generator.GetInt(0, wingPieces.Count));
+	}
+
+	void TogglePieces(List<Transform> list, int active) {
+		foreach (Transform t in list) {
+			t.gameObject.SetActive(false);
+		}
+
+		list[active].gameObject.SetActive(true);
+	}
+}
--- PCGDiorama/Assets/Scripts/Settlement.cs
+++ PCGDiorama/Assets/Scripts/Settlement.cs
@@ -15,58 +15,64 @@ public class Settlement : MonoBehaviour {
 		// int buildingTarget = generator.GetInt(3, 12);
 		int buildingTarget = 12;
 
-		for (int i = 0; i < buildingTarget; i++) {
-			GameObject b = GameObject.Instantiate(Resources.Load(buildingPrefabs[generator.GetInt(0, buildingPrefabs.Length)])) as GameObject;
-			b.transform.parent = transform;
-			b.transform.localPosition = new Vector3(generator.GetFloat(-35.0f, 35.0f), 4.0f, generator.GetFloat(-35.0f, 35.0f));
-			buildings.Add(b.GetComponent<Building>());
-		}
-
-		// foreach (Building b in buildings) {
-		// 	foreach (Building other in buildings) {
-		// 		if (b != other) {
-		// 			// if (RectA.Left < RectB.Right &&
-		// 			// 	RectA.Right > RectB.Left &&
-		// 			// 	RectA.Top > RectB.Bottom &&
-		// 			// 	RectA.Bottom < RectB.Top )
+		// while (buildings.Count < buildingTarget) {
+		// 	for (int i = 0; i < buildingTarget; i++) {
+		// 		GameObject b = GameObject.Instantiate(Resources.Load(buildingPrefabs[generator.GetInt(0, buildingPrefabs.Length)])) as GameObject;
+		// 		b.transform.parent = transform;
+		// 		b.transform.localPosition = new Vector3(generator.GetFloat(-35.0f, 35.0f), 4.0f, generator.GetFloat(-35.0f, 35.0f));
+		// 		buildings.Add(b.GetComponent<Building>());
+		// 	}
 		//
+		// 	for (int i = 0; i < buildings.Count; i++) {
+		// 		bool foundOverlap = false;
 		//
-		// 			if (b.transform.position.x - b.width < other.transform.position.x + other.width &&
-		// 				b.transform.position.x + b.width > other.transform.position.x - other.width &&
-		// 				b.transform.position.z - b.length < other.transform.position.z + other.length &&
-		// 				b.transform.position.z + b.length > other.transform.position.z - other.length)
-		// 			{
-		// 				// b.transform.GetChild(0).GetComponent<Renderer>().material.color = Color.red;
-		// 				other.transform.GetChild(0).GetComponent<Renderer>().material.color = Color.red;
+		// 		for (int j = 0; j < buildings.Count; j++) {
+		// 			if (i != j) {
+		//
+		// 				Building b = buildings[i];
+		// 				Building other = buildings[j];
+		//
+		// 				if (b.transform.position.x - (b.width / 2) < other.transform.position.x + (other.width / 2) &&
+		// 					b.transform.position.x + (b.width / 2) > other.transform.position.x - (other.width / 2) &&
+		// 					b.transform.position.z - (b.length / 2) < other.transform.position.z + (other.length / 2) &&
+		// 					b.transform.position.z + (b.length / 2) > other.transform.position.z - (other.length / 2))
+		// 				{
+		// 					foundOverlap = true;
+		// 				}
 		// 			}
+		// 		}
+		//
+		// 		if (foundOverlap) {
+		// 			// buildings[i].transform.GetChild(0).GetComponent<Renderer>().material.color = Color.red;
+		// 			Destory(buidlings[i].gameObject);
+		// 		} else {
+		// 			buildings[i].transform.GetChild(0).GetComponent<Renderer>().material.color = Color.green;
 		// 		}
 		// 	}
 		// }
 
-		for (int i = 0; i < buildings.Count; i++) {
+		while (buildings.Count < buildingTarget) {
+			GameObject b = GameObject.Instantiate(Resources.Load(buildingPrefabs[generator.GetInt(0, buildingPrefabs.Length)])) as GameObject;
+			b.transform.parent = transform;
+			b.transform.localPosition = new Vector3(generator.GetFloat(-35.0f, 35.0f), 4.0f, generator.GetFloat(-35.0f, 35.0f));
 
 			bool foundOverlap = false;
+			for (int i = 0; i < buildings.Count; i++) {
+				Building other = buildings[i];
 
-			for (int j = 0; j < buildings.Count; j++) {
-				if (i != j) {
-
-					Building b = buildings[i];
-					Building other = buildings[j];
-
-					if (b.transform.position.x - (b.width / 2) < other.transform.position.x + (other.width / 2) &&
-						b.transform.position.x + (b.width / 2) > other.transform.position.x - (other.width / 2) &&
-						b.transform.position.z - (b.length / 2) < other.transform.position.z + (other.length / 2) &&
-						b.transform.position.z + (b.length / 2) > other.transform.position.z - (other.length / 2))
-					{
-						foundOverlap = true;
-					}
+				if (b.transform.position.x - (b.GetComponent<Building>().width / 2) < other.transform.position.x + (other.width / 2) &&
+					b.transform.position.x + (b.GetComponent<Building>().width / 2) > other.transform.position.x - (other.width / 2) &&
+					b.transform.position.z - (b.GetComponent<Building>().length / 2) < other.transform.position.z + (other.length / 2) &&
+					b.transform.position.z + (b.GetComponent<Building>().length / 2) > other.transform.position.z - (other.length / 2))
+				{
+					foundOverlap = true;
 				}
 			}
 
-			if (foundOverlap) {
-				buildings[i].transform.GetChild(0).GetComponent<Renderer>().material.color = Color.red;
+			if (!foundOverlap) {
+				buildings.Add(b.GetComponent<Building>());
 			} else {
-				buildings[i].transform.GetChild(0).GetComponent<Renderer>().material.color = Color.green;
+				Destroy(b);
 			}
 		}
 	}
